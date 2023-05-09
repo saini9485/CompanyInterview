925863699106878fe5460c37b5bb92f6dbd2fe01
Rajesh Saini <saini222302@gmail.com>
2023-05-09 16:23:18 +0530

    ResourcePlane Interview

---
+++ PlaneResource/ResourcePlan.jsx
@@ -0,0 +1,21 @@
+const projects = [
+  {
+    id: 1,
+    name: "Project A",
+    timeEntries: [
+      { date: "2023-01-01", hours: 8 },
+      { date: "2023-01-02", hours: 6 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Project B",
+    timeEntries: [
+      { date: "2023-01-01", hours: 4 },
+      { date: "2023-01-02", hours: 8 },
+      { date: "2023-01-02", hours: 5 },
+    ],
+  },
+];
+
+//   write a funcion how we can show top 5 project by culculating maximum hours  if we have multiple projets
